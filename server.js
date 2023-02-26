86ecab74c3e06adb761222beb662d0417e04eb33
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-02-26 14:25:43 +0200

    added (CRUD) Categories

--- server.js
+++ server.js
@@ -1,41 +1,45 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
-const mongoose = require("mongoose");
-const morgan = require("morgan");
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 
-const Product = require('./models/product')
-require("dotenv").config();
+const Product = require('./models/product');
+
+require('dotenv').config();
 
 const api = process.env.API_URL;
 
-const productRouter = require('./routers/product.js')
+const productRouter = require('./routers/product.js');
+
+const catgeoryRouter = require('./routers/categories.js');
 
 //Middleware
 app.use(bodyParser.json());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
 //Router
-app.use(`${api}/products`,productRouter)
+app.use(`${api}/products`, productRouter);
+app.use(`${api}/categories`, catgeoryRouter);
 
 const port = 3000;
-mongoose.set("strictQuery", true);
+mongoose.set('strictQuery', true);
 
 mongoose
-  .connect(process.env.CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: "eshop-database",
-  })
-  .then(() => {
-    console.log("Database connection established");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: 'eshop-database',
+    })
+    .then(() => {
+        console.log('Database connection established');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.listen(process.env.PORT || port, () => {
-  console.log(`server listening on port  ${port}`);
+    console.log(`server listening on port  ${port}`);
 });
