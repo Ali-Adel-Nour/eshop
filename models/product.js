9878df8be00b549404e56517c1bf132ff59ea358
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-02-27 22:54:31 +0200

    Setup Products & Categories

--- models/product.js
+++ models/product.js
@@ -62,5 +62,11 @@ const productSchema = mongoose.Schema({
         default: Date.now,
     },
 });
+productSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
 
+productSchema.set('toJSON', {
+    virtuals: true,
+});
 module.exports.Product = mongoose.model('Product', productSchema);
